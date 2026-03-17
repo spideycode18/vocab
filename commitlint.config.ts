77e0061f26838619eec89acd335495e5adb17e1a
spideycode <spideycode18@gmail.com>
2026-03-17 17:23:25 +0700

    chore: update config

---
+++ commitlint.config.ts
@@ -0,0 +1,31 @@
+import type { UserConfig } from '@commitlint/types';
+
+const config: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'perf',
+        'ci',
+        'revert',
+        'build',
+      ],
+    ],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-max-length': [2, 'always', 72],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'body-max-line-length': [1, 'always', 100],
+  },
+};
+
+export default config;
