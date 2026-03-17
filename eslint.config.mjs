2c8104bb8b442436c1bd3e508660ae9e24f7a669
spideycode <spideycode18@gmail.com>
2026-03-17 23:26:23 +0700

    fix: format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // eslint.config.mjs
-import { defineConfig, globalIgnores } from 'eslint/config'
-import nextVitals from 'eslint-config-next/core-web-vitals'
-import prettier from 'eslint-config-prettier/flat'
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import prettier from 'eslint-config-prettier/flat';
 
 export default defineConfig([
   ...nextVitals,
@@ -20,12 +20,15 @@ export default defineConfig([
     rules: {
       'react-hooks/exhaustive-deps': 'error',
       '@next/next/no-img-element': 'error',
-      'react/react-in-jsx-scope': 'off',   // React 17+ JSX transform
-      'react/prop-types': 'off',            // TypeScript thay thế
-      '@typescript-eslint/no-unused-vars': ['error', {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-      }],
+      'react/react-in-jsx-scope': 'off', // React 17+ JSX transform
+      'react/prop-types': 'off', // TypeScript thay thế
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/consistent-type-imports': 'error',
       '@typescript-eslint/no-empty-object-type': 'error',
@@ -34,4 +37,4 @@ export default defineConfig([
       'import/no-anonymous-default-export': 'warn',
     },
   },
-])
+]);
